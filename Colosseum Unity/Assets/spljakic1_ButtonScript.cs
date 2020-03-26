92fd187d2e639fc4d6ab37c474205407f07dde1c
sejlap <sejlapljakic233@gmail.com>
2020-03-26 22:14:16 +0100

    Dodan citav projekat i mobilna app

---
+++ Colosseum Unity/Assets/spljakic1_ButtonScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spljakic1_ButtonScript : MonoBehaviour {
+
+ 
+
+
+
+
+    public GameObject Panel;
+
+
+    public void OpenPanel()
+    {
+
+        if (Panel != null)
+        {
+            bool isActive = Panel.activeSelf;
+
+            Panel.SetActive(!isActive);
+        }
+
+    }
+
+
+    public void izlaz()
+    { 
+         Application.Quit();
+    }
+}
---
+++ Colosseum Unity/Assets/spljakic1_skripta.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spljakic1_skripta : MonoBehaviour
+{
+
+    Ray ray;
+    RaycastHit hit;
+
+    public GameObject spljakic1_eagle;
+    public GameObject spljakic1_eagle1;
+    public GameObject spljakic1_eagle2;
+    public GameObject spljakic1_video;
+    public GameObject spljakic1_slike;
+    public GameObject spljakic1_tekst;
+  
+    // Use this for initialization
+    void Start()
+    {
+        spljakic1_video.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        spljakic1_slike.SetActive(false);
+        spljakic1_tekst.SetActive(false);
+      
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.collider.gameObject.name == "spljakic1_eagle")
+            {
+                spljakic1_video.SetActive(true);
+                spljakic1_slike.SetActive(false);
+                spljakic1_tekst.SetActive(false);
+            }
+            if (hit.collider.gameObject.name == "spljakic1_eagle1")
+            {
+                spljakic1_tekst.SetActive(true);
+                spljakic1_video.SetActive(false);
+                spljakic1_slike.SetActive(false);
+            }
+            if (hit.collider.gameObject.name == "spljakic1_eagle2")
+            {
+                spljakic1_slike.SetActive(true);
+                spljakic1_tekst.SetActive(false);
+                spljakic1_video.SetActive(false);
+            }
+
+        } 
+    }
+
+}
+   
